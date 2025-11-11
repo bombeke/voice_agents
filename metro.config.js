c3b7d94db97eef8b3ecdbc329a02eed0ec583bcf
Alex Tumwesigye <atumwesigye@gmail.com>
2025-11-11 19:25:39 +0300

    Use github actions only

--- metro.config.js
+++ metro.config.js
@@ -5,17 +5,6 @@ const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
-// Fix aliases
-config.resolver.alias = {
-  ...config.resolver.alias,
-  "@": path.resolve(__dirname),
-};
-
-// Preserve default extensions (don't append duplicates)
-config.resolver.sourceExts = [
-  "ts", "tsx", "js", "jsx", "json"
-];
-
 // Required for NativeWind + Expo SDK 53
 config.transformer.unstable_allowRequireContext = true;
 
