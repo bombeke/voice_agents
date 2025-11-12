ea7cfe6795dbbc72133f71ba1c11e9ca25ae4d96
Alex Tumwesigye <atumwesigye@gmail.com>
2025-11-12 13:56:14 +0300

    Update libraries

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,7 @@
 // metro.config.js
 const { getDefaultConfig } = require("expo/metro-config");
 const { withUniwindConfig } = require("uniwind/metro");
-const path = require("path");
 
 const config = getDefaultConfig(__dirname);
-
-// Required for NativeWind + Expo SDK 53
-config.transformer.unstable_allowRequireContext = true;
-
-// CI-friendly symlinks (safe)
-config.resolver.unstable_enableSymlinks = true;
 
 module.exports = withUniwindConfig(config, { cssEntryFile: "./app/global.css" });
