274ed31a5bcbdd4d0ddf9c3f06629f3558ac9e06
Alex Tumwesigye <atumwesigye@gmail.com>
2025-11-28 14:03:57 +0300

    Fix AI detection

--- hooks/useCachedModel.ts
+++ hooks/useCachedModel.ts
@@ -1,7 +1,7 @@
 import { useTensorflowModel } from "react-native-fast-tflite"
 
 export const useCachedModel =()=>{
-    const model = useTensorflowModel(require('../yolo11n_float16.tflite'))
+    const model = useTensorflowModel(require('@assets/yolo11n_float16.tflite'))
     const actualModel = model.state === 'loaded' ? model.model : null
     return { 
         model: actualModel 
