0c290a8f1129c688b6863791fcac09e0cbff9004
Alex Tumwesigye <atumwesigye@gmail.com>
2025-12-04 21:08:04 +0300

    Fix model track

--- hooks/useCachedModel.ts
+++ hooks/useCachedModel.ts
@@ -2,10 +2,10 @@ import { useEffect, useState } from "react";
 import { useTensorflowModel } from "react-native-fast-tflite";
 
 export const useCachedModel =()=>{
-    //const {model, state} = useTensorflowModel(require('@/assets/ssd_mobilenet_v1.tflite'));
-    const { model, state } = useTensorflowModel(
+    const {model, state} = useTensorflowModel(require('@/assets/ssd_mobilenet_v1.tflite'));
+    /*const { model, state } = useTensorflowModel(
         require("../assets/ssd_mobilenet_v1.tflite")  
-    );
+    );*/
     
     const [cachedModel, setCachedModel] = useState<any | null>(null);
 
