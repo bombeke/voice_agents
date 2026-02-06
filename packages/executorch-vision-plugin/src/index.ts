fc8f19eb72e9c160b62f87bf82101e340ad7addb
Alex Tumwesigye <atumwesigye@gmail.com>
2026-02-06 12:09:44 +0300

    fix packages

--- packages/executorch-vision-plugin/src/index.ts
+++ packages/executorch-vision-plugin/src/index.ts
@@ -1,20 +1,23 @@
 // Reexport the native module. On web, it will be resolved to ExecutorchVisionPluginModule.web.ts
 // and on native platforms to ExecutorchVisionPluginModule.ts
-export { default } from './ExecutorchVisionPluginModule';
-export { default as ExecutorchVisionPluginView } from './ExecutorchVisionPluginView';
-export * from  './ExecutorchVisionPlugin.types';
-import { requireNativeModule } from 'expo-modules-core';
+export * from "./ExecutorchVisionPlugin.types";
+export { default } from "./ExecutorchVisionPluginModule";
+export { default as ExecutorchVisionPluginView } from "./ExecutorchVisionPluginView";
 
-import type { YoloModelConfig, Detection } from './types';
-export const ExpoYoloExecuTorchPlugin = requireNativeModule('ExecutorchVisionPlguinModule');
+import { requireNativeModule } from "expo-modules-core";
+import type { Detection, YoloModelConfig } from "./types";
+
+const NativeModule = requireNativeModule("ExecutorchVisionPluginModule");
+
+export const ExpoYoloExecuTorchPlugin = NativeModule;
 
 export function registerModels(models: YoloModelConfig[]) {
-  return Native.registerModels(models);
+  return NativeModule.registerModels(models);
 }
 
 export function getLatestDetections(): Detection[] {
-  return Native.getLatestDetections();
+  return NativeModule.getLatestDetections();
 }
 
 // VisionCamera will find the plugin by name automatically
-export const YOLO_FRAME_PROCESSOR_NAME = 'YoloFrameProcessor';
+export const YOLO_FRAME_PROCESSOR_NAME = "YoloFrameProcessor";
