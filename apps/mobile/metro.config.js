9adfcefce74eafbb14c313f506ce262bb3352116
Alex Tumwesigye <atumwesigye@gmail.com>
2026-02-18 11:01:20 +0300

    Remove duplicate packages

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -3,21 +3,21 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withUniwindConfig } = require("uniwind/metro");
 const path = require("path");
 
-const projectRoot = __dirname;
-//const config = getDefaultConfig(__dirname);
-const workspaceRoot = path.resolve(projectRoot, "../..");
+//const projectRoot = __dirname;
+const config = getDefaultConfig(__dirname);
+//const workspaceRoot = path.resolve(projectRoot, "../..");
 
-const config = getDefaultConfig(projectRoot);
+//const config = getDefaultConfig(projectRoot);
 config.resolver.assetExts.push("tflite", "bin", "onnx", "ncnn", "pte");
 
-config.watchFolders = [workspaceRoot];
+//config.watchFolders = [workspaceRoot];
 //config.watchFolders = [path.resolve(__dirname, "../../packages")];
 
-config.resolver.nodeModulesPaths = [
+/*config.resolver.nodeModulesPaths = [
   path.join(projectRoot, "node_modules"),
   path.join(workspaceRoot, "node_modules"),
 ];
 
 config.resolver.disableHierarchicalLookup = true;
-
+*/
 module.exports = withUniwindConfig(config, { cssEntryFile: "./global.css" });
