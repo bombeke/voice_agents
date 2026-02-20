d42f03de8c380af41b2132b88529df537f4a4124
Alex Tumwesigye <atumwesigye@gmail.com>
2026-02-20 13:00:45 +0300

    Remove duplicate packages

--- apps/mobile/hooks/usePoleDetection.ts
+++ apps/mobile/hooks/usePoleDetection.ts
@@ -2,7 +2,7 @@ import { useRef, useState } from "react";
 import { Dimensions } from "react-native";
 import { runOnJS, useSharedValue } from "react-native-reanimated";
 import { useFrameProcessor } from "react-native-vision-camera";
-import { detectTags } from "react-native-vision-camera-executorch";
+//import { detectTags } from "react-native-vision-camera-executorch";
 import { useResizePlugin } from "vision-camera-resize-plugin";
 //import { useCachedModel } from "./useCachedModel";
 
@@ -121,11 +121,11 @@ export const runModelInferenceJS = (
     */
 
     //console.log("Inference result:", detections);
-    const result = detectTags(frame, {
+    /*const result = detectTags(frame, {
       modelPath: "/data/local/tmp/model.pte",
     });
     console.log("Inference result2:", result);
-
+    */
     inferenceResult.value = detections ?? [];
     return detections ?? [];
   } catch (e) {
