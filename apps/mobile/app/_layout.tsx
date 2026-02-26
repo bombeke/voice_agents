d8772dde610e26ce96e0e3d80005570dcb4ca59e
Alex Tumwesigye <atumwesigye@gmail.com>
2026-02-26 23:50:43 +0300

    Fix detection

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -17,6 +17,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import * as SplashScreen from "expo-splash-screen";
 import { StyleSheet } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import "react-native-vision-camera-executorch";
 
 SplashScreen.preventAutoHideAsync();
 
